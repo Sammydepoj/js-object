57ff8ba351f24eb732397b72e74d0f5e352e16a4
Sammydepoj <97622819+Sammydepoj@users.noreply.github.com>
2022-07-01 22:21:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,115 @@
+const workers = {
+workers1:{
+serial_Number:1,FirstName:"Thompson",Lastname:"Anthony",Age:32,sex:"Male",      position:"Head of finance department",Date_of_employement:"17/07/2014"},
+workers2:{
+serial_Number:2,FirstName:"Johnson",Lastname:"Mary",Age:23,sex:"Female",position:"Software Engineer",Date_of_employement:"05/03/2019"},
+workers3:{
+serial_Number:3,FirstName:"Chisom",Lastname:"Onyeka",Age:28,sex:"Female",position:"Human Resource Manager",Date_of_employement:"21/06/2016"},
+workers4:{
+serial_Number:4,FirstName:"Jide",Lastname:"Olabisi",Age:29,sex:"Male",position:"Assistant Head (Software department)",Date_of_employement:"05/06/2016"},
+workers5:{
+serial_Number:5,FirstName:"Williams",Lastname:"Ajayi",Age:30,sex:"Male",position:"Head of Software department",Date_of_employement:"06/09/2015"},
+workers6:{
+ serial_Number:6,FirstName:"Nancy",Lastname:"Chukwuemeka",Age:27,sex:"Female",position:"Secretary",Date_of_employement:"23/08/2016"},
+workers7:{
+serial_Number:7,FirstName:"Nicholas",Lastname:"Uchemba",Age:26,sex:"Male",position:"Software Engineer",Date_of_employement:"22/02/2020"},
+workers8:{
+serial_Number:7,FirstName:"Ahmed",Lastname:"Olayinka",Age:24,sex:"Male",position:"Graphic Designer",Date_of_employement:"15/08/2020"},
+ workers9:{
+serial_Number:9,FirstName:"Halima",Lastname:"Mohammed",Age:26,sex:"Female",position:"Senior finance Manager",Date_of_employement:"30/05/2018"},
+workers10:{
+  serial_Number:10,FirstName:"Richard",Lastname:"Adeyemi",Age:41,sex:"Male",position:"C.E.O.",Date_of_employement:"17/11/2010"}
+            
+}
+document.getElementById("sn").innerHTML=workers.workers1.serial_Number;
+document.getElementById("fname").innerHTML=workers.workers1.FirstName;
+document.getElementById("lname").innerHTML=workers.workers1.Lastname;
+document.getElementById("age").innerHTML=workers.workers1.Age;
+document.getElementById("sex").innerHTML=workers.workers1.sex;
+document.getElementById("position").innerHTML=workers.workers1.position;
+document.getElementById("doe").innerHTML=workers.workers1.Date_of_employement;
+
+
+
+document.getElementById("sn2").innerHTML=workers.workers2.serial_Number;
+document.getElementById("fname2").innerHTML=workers.workers2.FirstName;
+document.getElementById("lname2").innerHTML=workers.workers2.Lastname;
+document.getElementById("age2").innerHTML=workers.workers2.Age;
+document.getElementById("sex2").innerHTML=workers.workers2.sex;
+document.getElementById("position2").innerHTML=workers.workers2.position;
+document.getElementById("doe2").innerHTML=workers.workers2.Date_of_employement;
+
+
+document.getElementById("sn3").innerHTML=workers.workers3.serial_Number;
+document.getElementById("fname3").innerHTML=workers.workers3.FirstName;
+document.getElementById("lname3").innerHTML=workers.workers3.Lastname;
+document.getElementById("age3").innerHTML=workers.workers3.Age;
+document.getElementById("sex3").innerHTML=workers.workers3.sex;
+document.getElementById("position3").innerHTML=workers.workers3.position;
+document.getElementById("doe3").innerHTML=workers.workers3.Date_of_employement;
+
+
+
+document.getElementById("sn4").innerHTML=workers.workers4.serial_Number;
+document.getElementById("fname4").innerHTML=workers.workers4.FirstName;
+document.getElementById("lname4").innerHTML=workers.workers4.Lastname;
+document.getElementById("age4").innerHTML=workers.workers4.Age;
+document.getElementById("sex4").innerHTML=workers.workers4.sex;
+document.getElementById("position4").innerHTML=workers.workers4.position;
+document.getElementById("doe4").innerHTML=workers.workers4.Date_of_employement;
+
+
+document.getElementById("sn5").innerHTML=workers.workers5.serial_Number;
+document.getElementById("fname5").innerHTML=workers.workers5.FirstName;
+document.getElementById("lname5").innerHTML=workers.workers5.Lastname;
+document.getElementById("age5").innerHTML=workers.workers5.Age;
+document.getElementById("sex5").innerHTML=workers.workers5.sex;
+document.getElementById("position5").innerHTML=workers.workers5.position;
+document.getElementById("doe5").innerHTML=workers.workers5.Date_of_employement;
+
+document.getElementById("sn6").innerHTML=workers.workers6.serial_Number;
+document.getElementById("fname6").innerHTML=workers.workers6.FirstName;
+document.getElementById("lname6").innerHTML=workers.workers6.Lastname;
+document.getElementById("age6").innerHTML=workers.workers6.Age;
+document.getElementById("sex6").innerHTML=workers.workers6.sex;
+document.getElementById("position6").innerHTML=workers.workers6.position;
+document.getElementById("doe6").innerHTML=workers.workers6.Date_of_employement;
+
+
+
+document.getElementById("sn7").innerHTML=workers.workers7.serial_Number;
+document.getElementById("fname7").innerHTML=workers.workers7.FirstName;
+document.getElementById("lname7").innerHTML=workers.workers7.Lastname;
+document.getElementById("age7").innerHTML=workers.workers7.Age;
+document.getElementById("sex7").innerHTML=workers.workers7.sex;
+document.getElementById("position7").innerHTML=workers.workers7.position;
+document.getElementById("doe7").innerHTML=workers.workers7.Date_of_employement;
+
+
+
+document.getElementById("sn8").innerHTML=workers.workers8.serial_Number;
+document.getElementById("fname8").innerHTML=workers.workers8.FirstName;
+document.getElementById("lname8").innerHTML=workers.workers8.Lastname;
+document.getElementById("age8").innerHTML=workers.workers8.Age;
+document.getElementById("sex8").innerHTML=workers.workers8.sex;
+document.getElementById("position8").innerHTML=workers.workers8.position;
+document.getElementById("doe8").innerHTML=workers.workers8.Date_of_employement;
+
+
+document.getElementById("sn9").innerHTML=workers.workers9.serial_Number;
+document.getElementById("fname9").innerHTML=workers.workers9.FirstName;
+document.getElementById("lname9").innerHTML=workers.workers9.Lastname;
+document.getElementById("age9").innerHTML=workers.workers9.Age;
+document.getElementById("sex9").innerHTML=workers.workers9.sex;
+document.getElementById("position9").innerHTML=workers.workers9.position;
+document.getElementById("doe9").innerHTML=workers.workers9.Date_of_employement;
+
+document.getElementById("sn0").innerHTML=workers.workers10.serial_Number;
+document.getElementById("fname0").innerHTML=workers.workers10.FirstName;
+document.getElementById("lname0").innerHTML=workers.workers10.Lastname;
+document.getElementById("age0").innerHTML=workers.workers10.Age;
+document.getElementById("sex0").innerHTML=workers.workers10.sex;
+document.getElementById("position0").innerHTML=workers.workers10.position;
+document.getElementById("doe0").innerHTML=workers.workers10.Date_of_employement;
+
+
